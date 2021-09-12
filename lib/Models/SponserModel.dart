b9599beccdea62813914b0ea1598ba7d759d8187
Moaaz Mohammed <84313242+Moaaz-Mohammed@users.noreply.github.com>
2021-09-12 18:39:43 +0200

    .

---
+++ lib/Models/SponserModel.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SponserModel extends StatelessWidget {
+  const SponserModel({
+    Key? key,
+    required this.CompanyImg,
+    required this.CompanyName,
+    required this.CompanyWebsite,
+
+  }) : super(key: key);
+
+  final String CompanyImg;
+  final String CompanyName;
+  final String CompanyWebsite;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
