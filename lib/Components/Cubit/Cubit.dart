d9a480df81edc17c9e50f0a0127d27cbcb3acb7c
Moaaz Mohammed <84313242+Moaaz-Mohammed@users.noreply.github.com>
2021-09-24 17:21:24 +0200

    .

--- lib/Components/Cubit/Cubit.dart
+++ lib/Components/Cubit/Cubit.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'States.dart';
 
@@ -10,7 +9,6 @@ class AppCubit extends Cubit<AppStates> {
   static AppCubit get(context) => BlocProvider.of(context);
 
   bool isLoaded = true;
-
 
   void LoadingPosts() {
     Future.delayed(Duration(seconds: 2),(){
